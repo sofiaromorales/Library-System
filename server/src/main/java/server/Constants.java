9f319205a5e93920a3dfad608e3225e454fa0d5a
Sofia <sofia@Mac-mini-de-Corina.local>
2021-12-10 11:02:18 -0400

    constants fixed

--- server/src/main/java/server/Constants.java
+++ server/src/main/java/server/Constants.java
@@ -11,6 +11,7 @@ package server;
 public class Constants {
     public static final String LIBRARY_SERVER = "";
     public static final String CLIENT_IP = "localhost";
+    public static final String SERVER_IP = "localhost";
     public static final Integer SOCKET_REMOTE_LIBRARY_PORT = 6666;
     public static final String LOG_FILE_PATH = "C:\\Users\\LosSanchez\\Documents\\GitHub\\Library-System\\server\\src\\main\\java\\server\\log.txt";
 }
